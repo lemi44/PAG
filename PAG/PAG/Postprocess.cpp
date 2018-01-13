fa5451c6c8ff960a4553938105b68165c912138c
lemi44 <lemi44@gmail.com>
2018-01-13 11:19:28 +0100

    PAG6 [WIP] - fix to auto-exposure

--- PAG/PAG/Postprocess.cpp
+++ PAG/PAG/Postprocess.cpp
@@ -141,6 +141,7 @@ float Postprocess::findAverage(float prevExposure)
 	glGetTexImage(GL_TEXTURE_2D, max_mipmap, GL_RGB, GL_FLOAT, &luminescence[0]);
 
 	GLfloat lum = 0.2126 * luminescence[0] + 0.7152 * luminescence[1] + 0.0722 * luminescence[2];
+	if (lum < 0.001f) lum = 0.001f;
 	auto ret = lerp(prevExposure, 0.5 / lum, 0.05); // slowly adjust exposure based on average brightness
 
 	return ret;
